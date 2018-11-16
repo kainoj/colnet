1dd3c76a57b76da9199336876cfdb32453ae7804
kainoj <przemko924@gmail.com>
2018-11-16 11:04:19 +0100

    ColNet refactored: now self.Conv2d() returns instance of nn.Conv2d()

--- src/colnet.py
+++ src/colnet.py
@@ -7,7 +7,12 @@ import numpy as np
 class ColNet(nn.Module):
     """Colorization network class"""
 
-    
+    def Conv2d(self, in_ch, out_ch, stride):
+        """Returns an instance of nn.Conv2d"""
+        return nn.Conv2d(in_channels=in_ch, out_channels=out_ch,
+                         stride=stride, kernel_size=3, padding=1)
+
+
     def __init__(self, net_size=1):
         """Initializes the network.
 
@@ -19,33 +24,34 @@ class ColNet(nn.Module):
         self.net_size = net_size
 
 
-        ksize = np.array( [1, 64, 128, 128, 256, 256, 512, 512, 256, 128, 64, 64, 32] ) // self.net_size
+        ksize = np.array([1, 64, 128, 128, 256, 256, 512,
+                          512, 256, 128, 64, 64, 32]) // self.net_size
         ksize[0] = 1
         
         # 'Low-level features'
-        self.conv1 = nn.Conv2d(in_channels=1,        out_channels=ksize[1], kernel_size=3, stride=2, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=ksize[1], out_channels=ksize[2], kernel_size=3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(in_channels=ksize[2], out_channels=ksize[3], kernel_size=3, stride=2, padding=1)
-        self.conv4 = nn.Conv2d(in_channels=ksize[3], out_channels=ksize[4], kernel_size=3, stride=1, padding=1)
-        self.conv5 = nn.Conv2d(in_channels=ksize[4], out_channels=ksize[5], kernel_size=3, stride=2, padding=1)
-        self.conv6 = nn.Conv2d(in_channels=ksize[5], out_channels=ksize[6], kernel_size=3, stride=1, padding=1)
+        self.conv1 = self.Conv2d(1, ksize[1], 2)
+        self.conv2 = self.Conv2d(ksize[1], ksize[2], 1)
+        self.conv3 = self.Conv2d(ksize[2], ksize[3], 2)
+        self.conv4 = self.Conv2d(ksize[3], ksize[4], 1)
+        self.conv5 = self.Conv2d(ksize[4], ksize[5], 2)
+        self.conv6 = self.Conv2d(ksize[5], ksize[6], 1)
         
         # 'Mid-level fetures'
-        self.conv7 = nn.Conv2d(in_channels=ksize[6], out_channels=ksize[7], kernel_size=3, stride=1, padding=1)
-        self.conv8 = nn.Conv2d(in_channels=ksize[7], out_channels=ksize[8], kernel_size=3, stride=1, padding=1)
+        self.conv7 = self.Conv2d(ksize[6], ksize[7], 1)
+        self.conv8 = self.Conv2d(ksize[7], ksize[8], 1)
         
         # 'Colorization network'
-        self.conv9 = nn.Conv2d(in_channels=ksize[8], out_channels=ksize[9], kernel_size=3, stride=1, padding=1)
+        self.conv9 = self.Conv2d(ksize[8], ksize[9], 1)
         
         # Here comes upsample #1
         
-        self.conv10 = nn.Conv2d(in_channels=ksize[9], out_channels=ksize[10], kernel_size=3, stride=1, padding=1)
-        self.conv11 = nn.Conv2d(in_channels=ksize[10],out_channels=ksize[11], kernel_size=3, stride=1, padding=1)
+        self.conv10 = self.Conv2d(ksize[9], ksize[10], 1)
+        self.conv11 = self.Conv2d(ksize[10], ksize[11], 1)
         
         # Here comes upsample #2        
         
-        self.conv12 = nn.Conv2d(in_channels=ksize[11], out_channels=ksize[12], kernel_size=3, stride=1, padding=1)
-        self.conv13 = nn.Conv2d(in_channels=ksize[12], out_channels=2, kernel_size=3, stride=1, padding=1)
+        self.conv12 = self.Conv2d(ksize[11], ksize[12], 1)
+        self.conv13 = self.Conv2d(ksize[12], 2, 1)
         
         
     def forward(self, x):
@@ -77,20 +83,20 @@ class ColNet(nn.Module):
         
         # assert out.shape[1:] == (128, 28, 28), "おわり： conv9"
         
-        out = nn.functional.interpolate(input=out, scale_factor=2, mode='nearest')
+        out = nn.functional.interpolate(input=out, scale_factor=2)
 
         # assert out.shape[1:] == (128, 56, 56), "おわり： upsample1"
     
         out = F.relu(self.conv10(out))
         out = F.relu(self.conv11(out))
         
-        out = nn.functional.interpolate(input=out, scale_factor=2, mode='nearest')
+        out = nn.functional.interpolate(input=out, scale_factor=2)
 
 
         out = F.relu(self.conv12(out))
         out = torch.sigmoid(self.conv13(out))
         
-        out = nn.functional.interpolate(input=out, scale_factor=2, mode='nearest')
+        out = nn.functional.interpolate(input=out, scale_factor=2)
         
         # assert out.shape[1:] == (2, 224, 224)
         
