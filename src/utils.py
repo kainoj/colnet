f2c79b5e1acb9deca170832c6c419c4e6f9e80b5
i282751 <i282751@lab110-08.stud.ii>
2018-11-28 15:52:29 +0100

    Bug fix: conversion net output to rgb image

--- src/utils.py
+++ src/utils.py
@@ -16,8 +16,8 @@ def net_out2rgb(L, ab_out):
         3 channel RGB image
     """
     # Convert to numpy and unnnormalize
-    L = (L.numpy() * 100.0).astype(np.int8)
-    ab_out = (np.floor(ab_out.numpy() * 255.0) - 128.0).astype(np.int8)
+    L = L.numpy() * 100.0
+    ab_out = ab_out.numpy() * 255.0 - 128.0
     
     
     # L and ab_out are tenosr i.e. are of shape of
@@ -29,4 +29,12 @@ def net_out2rgb(L, ab_out):
     # Stack layers  
     img_stack = np.dstack((L, ab_out))
     
-    return color.lab2rgb(img_stack)
+    # This line is CRUCIALL
+    #   - torch requires tensors to be float32
+    #   - thus all above (L, ab) are float32
+    #   - scikit image floats are in range -1 to 1
+    #   - http://scikit-image.org/docs/dev/user_guide/data_types.html
+    #   - idk what's next, but converting image to float64 somehow
+    #     does the job. scikit automagically converts those values.
+    img_stack = img_stack.astype(np.float64)	
+    return  color.lab2rgb(img_stack)
